1f772bba6870fa37a9251806ddf25dc19240a1ee
lymvs <lysandros.mavropoulos@gmail.com>
2025-05-06 21:51:06 +0000

    Add Cell class

--- src/main.py
+++ src/main.py
@@ -1,14 +1,25 @@
 from gui import Window, Point, Line
+from cell import Cell
 
 
 def main() -> None:
     win = Window(800, 600)
 
-    point1 = Point(50, 50)
-    point2 = Point(200, 100)
-    l = Line(point1, point2)
-    
-    win.draw_line(l, "red")
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
 
     win.wait_for_close()
 
